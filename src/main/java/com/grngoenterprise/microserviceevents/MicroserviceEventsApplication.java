f971fd4424dbb380f67f7e64cd046e665af2c016
Railson Magalhaes <railson.sistemas@gmail.com>
2023-12-01 09:09:17 -0300

    Iniciando Projeto

---
+++ src/main/java/com/grngoenterprise/microserviceevents/MicroserviceEventsApplication.java
@@ -0,0 +1,13 @@
+package com.grngoenterprise.microserviceevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grngoenterprise/microserviceevents/MicroserviceEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grngoenterprise.microserviceevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
